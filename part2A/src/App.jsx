4d7b4a7f52699a8d18558609ae3cb4fa98169b29
EvanaSabatella1989 <eva.sabatella@gmail.com>
2024-08-05 21:07:39 -0300

    Ejercicios parte 2a ejercicio 2.5

--- part2A/src/App.jsx
+++ part2A/src/App.jsx
@@ -1,33 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ parts }) => {
-  let totalExercises = parts.reduce((sum,part) => sum + part.exercises,0)
-  return(
-    <strong>Total of {totalExercises} exercises </strong>
-  )
-}
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    {parts.map(part => (
-      <Part key={part.id} part={part} />
-    ))}     
-  </>
-
-const Course = ({course}) => {
-  return (
-    <>
-    <Header course = {course.name}/>
-    <Content parts = {course.parts} />
-    <Total parts = {course.parts} />
-    </>
-  )
-}
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
